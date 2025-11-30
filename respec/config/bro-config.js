61e1e3cd8905ed7b94579e4c9573a6203c674b5d
Matthijs Hovestad <matthijs_hovestad@hotmail.com>
2025-11-30 22:50:54 +0100

    Refactor organisationConfig for improved clarity

--- respec/config/bro-config.js
+++ respec/config/bro-config.js
@@ -1,175 +1,179 @@
-// Dit bestand is afgeleid van het geonovum-config bestand, gepubliceerd als https://tools.geostandaarden.nl/respec/config/geonovum-config.js, voor hergebruik in BRO ReSpec documenten.
-var organisationConfig = {
-    nl_organisationName: "BRO",
-    // werkt nog niet 
-    // nl_organisationStylesURL: "https://tools.geostandaarden.nl/respec/gn-algemeen/style/",
-    // bv styles url voor ander GN publicatiedomein met eigen stijlen zoals DSO of BRO:
-    // nl_organisationStylesURL: "https://tools.geostandaarden.nl/respec/dso/style/",
-    // nl_organisationStylesURL: "https://tools.geostandaarden.nl/respec/bro/style/",
-    nl_organisationPublishURL: "https://docs.geostandaarden.nl/",
-    logos: [{
-    src: "https://tools.geostandaarden.nl/respec/media/BRO/logo_vro_blauw.png",
-    alt: "Ministerie van Volkshuisvesting en Ruimtelijke Ordering",
-    id: "logo_vro_blauw",
-    width: 776,
-    height: 163,
-    url: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
-    },
-	    ],
-	
-	postProcess: [window.respecMermaid.createFigures],
+// Zorg dat we na ReSpec-rendering de HTML in de SOTD kunnen zetten (ReSpec escapt standaard tekstfragmenten).
+postProcess: [
+    window.respecMermaid.createFigures,
+    function setSotdHtml() {
+        try {
+            // bepaal taal (document.lang of default nl)
+            var lang = (document.documentElement && document.documentElement.lang) ? document.documentElement.lang.split('-')[0] : 'nl';
+            lang = (lang === 'en') ? 'en' : 'nl';
 
-    // NB dit gaat ervan uit dat shortName = naam van de repository. Maar dit zal niet altijd het geval zijn. We kunnen edDraftURI ook nog steeds in de config.js opnemen.
-	// edDraftURI: ["https://broprogramma.github.io", "/", "shortName"],
-	latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/"],
-    thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
-    prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
-    useLogo: true,
-    useLabel: true,
+            // ReSpec plaatst de SOTD-tekst meestal in #sotd > p
+            var p = document.querySelector('#sotd > p') || document.querySelector('#sotd');
+            if (!p) return;
 
-    license: "cc-by",
-    addSectionLinks: true,
+            var vv = organisationConfig.sotdText && organisationConfig.sotdText[lang] && organisationConfig.sotdText[lang].vv;
+            if (!vv) return;
 
-    localizationStrings: {
-        en: {
-            wv: "Editor's draft",
-            cv: "Candidate recommendation",
-            vv: "Proposed recommendation",
-            def: "Recommendation",
-            basis: "Document",
-            //eo: "Outdated version",
-            //tg: "Rescinded version",
-            no: "Norm",
-            st: "Standard",
-            im: "Information model",
-            pr: "Practical guideline",
-            hr: "Guide",
-            // goede vertaling ontbreekt nog voor 'werkafspraak'
-            wa: "Werkafspraak",
-            al: "General",
-            bd: "Governance documentation",
-            bp: "Best practice",
-        },
-        nl: {
-            wv: "Werkversie",
-            cv: "Consultatieversie",
-            vv: "Versie ter vaststelling",
-            def: "Vastgestelde versie",
-            basis: "Document",
-            //eo: "Verouderde versie",
-            //tg: "Teruggetrokken versie",
-            no: "Norm",
-            st: "Standaard",
-            im: "Informatiemodel",
-            pr: "Praktijkrichtlijn",
-            hr: "Handreiking",
-            wa: "Werkafspraak",
-            al: "Algemeen",
-            bd: "Beheerdocumentatie",
-            bp: "Best practice",
-        },
-    },
+            // zet de HTML direct (gewild/vertrouwd HTML in config)
+            p.innerHTML = vv;
+        } catch (err) {
+            console.error('setSotdHtml error', err);
+        }
+    }
+],
 
-    specTypeText: {
-        en: {
-            no: "Norm",
-            st: "Standard",
-            im: "Information model",
-            pr: "Guideline",
-            hr: "Guide",
-            wa: "Werkafspraak",
-            al: "General",
-            bd: "Governance documentation",
-            bp: "Best practice",
-        },
-        nl: {
-            no: "Norm",
-            st: "Standaard",
-            im: "Informatiemodel",
-            pr: "Praktijkrichtlijn",
-            hr: "Handreiking",
-            wa: "Werkafspraak",
-            al: "Algemeen",
-            bd: "Beheerdocumentatie",
-            bp: "Best practice",
-        },
-    },
+// NB dit gaat ervan uit dat shortName = naam van de repository. Maar dit zal niet altijd het geval zijn. We kunnen edDraftURI ook nog steeds in de config.js opnemen.
+// edDraftURI: ["https://broprogramma.github.io", "/", "shortName"],
+latestVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/"],
+thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
+prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
+useLogo: true,
+useLabel: true,
 
-    specStatusText: {
-        en: {
-            wv: "Editor's draft",
-            cv: "Candidate recommendation",
-            vv: "Proposed recommendation",
-            def: "Recommendation",
-            basis: "Document",
-            //eo: "Outdated version",
-            //tg: "Rescinded version",
-        },
-        nl: {
-            wv: "Werkversie",
-            cv: "Consultatieversie",
-            vv: "Versie ter vaststelling",
-            def: "Vastgestelde versie",
-            basis: "Document",
-            //eo: "Verouderde versie",
-            //tg: "Teruggetrokken versie",
-        },
-    },
+license: "cc-by",
+addSectionLinks: true,
 
-    sotdText: {
-        nl: {
-            sotd: "Status van dit document",
-            def: `Dit is de definitieve versie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd.`,
-            wv: `Dit is een werkversie die op elk moment kan worden gewijzigd, verwijderd of vervangen door andere documenten. Het is geen stabiel document.`,
-            cv: `Dit is een consultatieversie. Commentaar over dit document kan gestuurd worden naar `,
-            vv: `Dit is de definitieve conceptversie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd. <p><b>Let op:</b> De aanlever-, gebruiks- en terugmeldplicht als bedoeld in artikelen 9, 27 en 30 van de <a href=\"https://wetten.overheid.nl/BWBR0037095\">Wet basisregistratie ondergrond (Bro)</a> gelden als deze catalogus is opgenomen in de <a href=\"https://wetten.overheid.nl/BWBR0040482\">Regeling Bro</a>.</p>`,
-            basis: "Dit is een document zonder officiële status.",
-        },
-        en: {
-            sotd: "Status of this document",
-            def: `This is the definitive version of this document. Edits resulting from consultations have been applied.`,
-            wv: `This is a working draft that can be changed, removed or replaced by other documents at any time. It is not a stable document.`,
-            cv: `This is a stable draft, published for public comment. Comments regarding this document may be sent to `,
-            vv: `This is the final draft of this document. Edits resulting from consultations have been applied.`,
-            basis: "This document has no official standing.",
-        },
+localizationStrings: {
+    en: {
+        wv: "Editor's draft",
+        cv: "Candidate recommendation",
+        vv: "Proposed recommendation",
+        def: "Recommendation",
+        basis: "Document",
+        //eo: "Outdated version",
+        //tg: "Rescinded version",
+        no: "Norm",
+        st: "Standard",
+        im: "Information model",
+        pr: "Practical guideline",
+        hr: "Guide",
+        // goede vertaling ontbreekt nog voor 'werkafspraak'
+        wa: "Werkafspraak",
+        al: "General",
+        bd: "Governance documentation",
+        bp: "Best practice",
     },
+    nl: {
+        wv: "Werkversie",
+        cv: "Consultatieversie",
+        vv: "Versie ter vaststelling",
+        def: "Vastgestelde versie",
+        basis: "Document",
+        //eo: "Verouderde versie",
+        //tg: "Teruggetrokken versie",
+        no: "Norm",
+        st: "Standaard",
+        im: "Informatiemodel",
+        pr: "Praktijkrichtlijn",
+        hr: "Handreiking",
+        wa: "Werkafspraak",
+        al: "Algemeen",
+        bd: "Beheerdocumentatie",
+        bp: "Best practice",
+    },
+},
 
-    labelColor: {
-        def: "#045D9F",
-        wv: "#FF0000",
-        cv: "#045D9F",
-        vv: "#045D9F",
-        basis: "#80CC28",
+specTypeText: {
+    en: {
+        no: "Norm",
+        st: "Standard",
+        im: "Information model",
+        pr: "Guideline",
+        hr: "Guide",
+        wa: "Werkafspraak",
+        al: "General",
+        bd: "Governance documentation",
+        bp: "Best practice",
     },
-    
-    licenses: {
-        "cc0": {
-            name: "Creative Commons 0 Public Domain Dedication",
-            short: "CC0",
-            url: "https://creativecommons.org/publicdomain/zero/1.0/",
-            image: "https://tools.geostandaarden.nl/respec/style/logos/CC-Licentie.svg",
-        },
-        "cc-by": {
-            name: "Creative Commons Attribution 4.0 International Public License",
-            short: "CC-BY",
-            url: "https://creativecommons.org/licenses/by/4.0/legalcode",
-            image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by.svg",	   
-        },
-        "cc-by-nd": {
-            name: "Creative Commons Naamsvermelding-GeenAfgeleideWerken 4.0 Internationaal",
-            short: "CC-BY-ND",
-            url: "https://creativecommons.org/licenses/by-nd/4.0/legalcode.nl",
-            image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by-nd.svg",
-        },
+    nl: {
+        no: "Norm",
+        st: "Standaard",
+        im: "Informatiemodel",
+        pr: "Praktijkrichtlijn",
+        hr: "Handreiking",
+        wa: "Werkafspraak",
+        al: "Algemeen",
+        bd: "Beheerdocumentatie",
+        bp: "Best practice",
     },
+},
 
-    localBiblio: {
-        "SemVer": {
-            href: "https://semver.org",
-            title: "Semantic Versioning 2.0.0",
-            authors: ["T. Preston-Werner"],
-            date: "June 2013"
-        },
+specStatusText: {
+    en: {
+        wv: "Editor's draft",
+        cv: "Candidate recommendation",
+        vv: "Proposed recommendation",
+        def: "Recommendation",
+        basis: "Document",
+        //eo: "Outdated version",
+        //tg: "Rescinded version",
     },
-}
+    nl: {
+        wv: "Werkversie",
+        cv: "Consultatieversie",
+        vv: "Versie ter vaststelling",
+        def: "Vastgestelde versie",
+        basis: "Document",
+        //eo: "Verouderde versie",
+        //tg: "Teruggetrokken versie",
+    },
+},
+
+sotdText: {
+    nl: {
+        sotd: "Status van dit document",
+        def: `Dit is de definitieve versie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd.`,
+        wv: `Dit is een werkversie die op elk moment kan worden gewijzigd, verwijderd of vervangen door andere documenten. Het is geen stabiel document.`,
+        cv: `Dit is een consultatieversie. Commentaar over dit document kan gestuurd worden naar `,
+        // Let op: we gebruiken hier geen extra <p> wrapper omdat ReSpec al een <p> om de SOTD plaatst.
+        // Gebruik inline HTML (a, strong, em, ...) — deze wordt na render gezet door setSotdHtml()
+        vv: `Dit is de definitieve conceptversie van dit document. Wijzigingen naar aanleiding van consultaties zijn doorgevoerd. <strong>Let op:</strong> De aanlever-, gebruiks- en terugmeldplicht als bedoeld in artikelen 9, 27 en 30 van de <a href="https://wetten.overheid.nl/BWBR0037095">Wet basisregistratie ondergrond (Bro)</a> gelden als deze catalogus is opgenomen in de <a href="https://wetten.overheid.nl/BWBR0040482">Regeling Bro</a>.`,
+        basis: "Dit is een document zonder officiële status.",
+    },
+    en: {
+        sotd: "Status of this document",
+        def: `This is the definitive version of this document. Edits resulting from consultations have been applied.`,
+        wv: `This is a working draft that can be changed, removed or replaced by other documents at any time. It is not a stable document.`,
+        cv: `This is a stable draft, published for public comment. Comments regarding this document may be sent to `,
+        vv: `This is the final draft of this document. Edits resulting from consultations have been applied.`,
+        basis: "This document has no official standing.",
+    },
+},
+
+labelColor: {
+    def: "#045D9F",
+    wv: "#FF0000",
+    cv: "#045D9F",
+    vv: "#045D9F",
+    basis: "#80CC28",
+},
+
+licenses: {
+    "cc0": {
+        name: "Creative Commons 0 Public Domain Dedication",
+        short: "CC0",
+        url: "https://creativecommons.org/publicdomain/zero/1.0/",
+        image: "https://tools.geostandaarden.nl/respec/style/logos/CC-Licentie.svg",
+    },
+    "cc-by": {
+        name: "Creative Commons Attribution 4.0 International Public License",
+        short: "CC-BY",
+        url: "https://creativecommons.org/licenses/by/4.0/legalcode",
+        image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by.svg",	   
+    },
+    "cc-by-nd": {
+        name: "Creative Commons Naamsvermelding-GeenAfgeleideWerken 4.0 Internationaal",
+        short: "CC-BY-ND",
+        url: "https://creativecommons.org/licenses/by-nd/4.0/legalcode.nl",
+        image: "https://tools.geostandaarden.nl/respec/style/logos/cc-by-nd.svg",
+    },
+},
+
+localBiblio: {
+    "SemVer": {
+        href: "https://semver.org",
+        title: "Semantic Versioning 2.0.0",
+        authors: ["T. Preston-Werner"],
+        date: "June 2013"
+    },
+},
